e280f82ef4dbf06710e5f6265143782ecdc03745
abdalrahman-droubi <abdalrahman.drobi@outlook.com>
2023-03-18 02:56:58 -0700

    add

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # JS-extra
+[imge](./Screenshot%202023-03-18%20025344.png)
---
+++ extra.js
@@ -0,0 +1,49 @@
+// Q1: Evaluate each of the following Javascript expressions and show the value
+
+console.log(-9 * 3)
+console.log("value is " + 50)
+console.log(17 % 5)
+console.log(5 % 17)
+console.log(5 / 10)
+console.log(4 == 4)
+console.log(4 != 5)
+console.log(7 <= 8)
+console.log(Math.ceil(2.1) - Math.floor(3.9))
+console.log(Math.ceil(-2.1) - Math.floor(-3.9))
+
+
+// Q6: If you have a “num” variable,  Your task is to print:
+
+let num = 10
+switch (num) {
+    case 1:
+        console.log("ONE")
+        break;
+    case 2 :
+        console.log("TOW")
+        break;
+    case 3 :
+        console.log("THREE")
+        break;
+    case 4 :
+        console.log("FOUR")
+        break;
+    case 5 :
+        console.log("FIVE")
+        break;
+    case 6 :
+        console.log("SEX")
+        break;
+    case 7 :
+        console.log("SEVEN")
+        break;
+    case 8 :
+        console.log("EIGHT")
+        break;
+    case 9 :
+        console.log("NINE")
+        break;
+    default:
+        console.log("PLEASE TRY AGAIN")
+        break;
+}
